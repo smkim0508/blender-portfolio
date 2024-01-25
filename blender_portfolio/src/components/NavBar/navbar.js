fede8aa54ab2e31f1d9d5f230c31c7c9f31ba347
Sungmin Kim <sungminkim@Sungmins-MacBook-Pro.local>
2024-01-25 17:51:39 -0500

    navbar progress

--- blender_portfolio/src/components/NavBar/navbar.js
+++ blender_portfolio/src/components/NavBar/navbar.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import '../../styles/navbar.css'
+import '../../assets/styles/navbar.css'
+// import logo from '../../assets/images/main_logo.png'
+import { Link } from 'react-scroll';
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className="navbar">
+        {/* <img src={logo} alt="logo" className="logo"/> */}
+        <img src="https://placehold.co/150x150" alt="logo" className="logo"/>
+        <div className="desktopMenu">
+            <Link className="desktopMenuListItem">Home</Link>
+            <Link className="desktopMenuListItem">About</Link>
+            <Link className="desktopMenuListItem">Portfolio</Link>
+            <Link className="desktopMenuListItem">Contact</Link>
+        </div>
+        <button className="desktopMenuBtn">
+            <img src="" alt="" className="desktopMenuImg"/> Contact Me
+        </button>
+    </nav>
   )
 }
 
-export default navbar
+export default Navbar
